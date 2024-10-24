07b42ebb2ef8093a21ef5a51e2f49e84860f518d
Jota <damatojoel25@gmail.com>
2024-10-23 22:27:28 -0300

    edit2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: './', // Intenta comentar esta línea y vuelve a hacer el build
+  base: '/',
+  server: {
+    https: true,
+  },
 });
